83e8b0853e4c69e926ca4384d6b35b7a5e8b4c95
Yuxiang <mail@yuxiang.ren>
2019-04-17 16:32:32 +0800

    Update At 2019-04-17 16:32:32

--- push.sh
+++ push.sh
@@ -6,7 +6,8 @@ git add .
 echo -n "正在提交备注...，请填写备注（可空）:"
 read remarks
 if [ ! -n "$remarks" ];then
-    remarks="update: "$(date)
+    remarks="Update At "$(date +%F\ %T)
+    # remarks="Update At "$(date)
 fi
 git commit -m "$remarks"
 echo "正在开始提交代码..."
